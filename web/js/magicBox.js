f4edaf50945293966fdbbf5a86baca5c67b37351
Colin GILLE <colin.gille@jimywoo.fr>
2014-05-26 23:37:24 +0200

    Change: add magic box module

---
+++ web/js/magicBox.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var wa;
+if(wa === undefined)
+	wa = {};
+
+wa.MagicBox = new dejavu.Class.declare({
+	$name: 'magicBox',
+
+	_root: null,
+
+	initialize: function(root) {
+		if(root.length !== 1)
+		{
+			throw 'One an only one element should be provided to MagicBox';
+		}
+		
+		this._root = root;
+
+		// Bind
+		var myObj = this;
+		root.children().click(function() {
+			myObj.goToSlide($(this));
+		}.$member());
+	},
+
+	goToSlide: function(slide) {
+		this._root.children().addClass('off');
+		slide.removeClass('off');
+	}
+});
