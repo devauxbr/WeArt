f16ecd4143e9cc62e864fb6d2aeba810280ad9f4
Bruno Devaux <devaux.br@gmail.com>
2014-06-05 16:00:05 +0200

    delete deprecated test routes and methods from HomeController

--- src/Wa/FrontBundle/Controller/HomeController.php
+++ src/Wa/FrontBundle/Controller/HomeController.php
@@ -11,73 +11,8 @@ use Wa\FrontBundle\Form\IdeaType;
 
 class HomeController extends Controller {
 
-    public function listArticleAction() {
-
-        $em = $this->getDoctrine()->getManager();
-        $articleRepo = $em->getRepository('WaFrontBundle:Article');
-
-        $articles = $articleRepo->findAll();
-
-        $idea = new Idea;
-        $form = $this->createForm(new IdeaType, $idea);
-
-        return $this->render('WaFrontBundle:Home:index.html.twig', array('articles' => $articles, 'form' => $form->createView()));
-    }
-
     public function indexAction() {
         return $this->render('WaFrontBundle:Home:index.html.twig');
-    }
-
-    public function addArticleAction() {
-        $article = new Article();
-        $article->setTitle('Une autre news');
-        $article->setContent('Le contenu');
-        $article->setCreateDate(new \DateTime());
-        $article->setEditDate(new \DateTime());
-        $article->setPublished(true);
-
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($article);
-        $em->flush();
-
-        return $this->render('WaFrontBundle:Home:addArticle.html.twig');
-    }
-
-    public function testAction() {
-        $idea = new Idea();
-        $idea->setTitle('The title');
-        $idea->setDescription('dqfsdfjsfksdfjksdf');
-        $idea->setVoteNumber(0);
-
-        $discipline = new Discipline();
-        $discipline->setTitle('sdfhsdfohzi');
-        $discipline->setDescription('Something');
-
-        $idea->setDiscipline($discipline);
-
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($discipline);
-        $em->persist($idea);
-        $em->flush();
-
-        return $this->render('WaFrontBundle:Home:addArticle.html.twig');
-    }
-
-    public function createTestUserAction() {
-        $account = new Account();
-        $account->setUsername('bdevaux');
-        $account->setPassword('password');
-        $account->setEnabled(true);
-        $account->setRoles(array());
-        $account->setPoint(0);
-        $account->setEmail('devaux.br@gmail.com');
-
-
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($account);
-        $em->flush();
-
-        return $this->render('WaFrontBundle:Home:addArticle.html.twig');
     }
 
     public function addIdeaAction() {
